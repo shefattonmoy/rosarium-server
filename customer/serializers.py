fb0b93e08b04f77efa6d6ee176aac85889ba2a8e
shefattonmoy <shefathossain7@gmail.com>
2024-07-07 22:15:24 +0600

    user logout implemented

--- customer/serializers.py
+++ customer/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from . import models
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -6,3 +7,33 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Customer
         fields = '__all__'
+        
+class RegistrationSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(required = True)
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'confirm_password']
+        
+    def save(self):
+        username = self.validated_data['username']
+        first_name = self.validated_data['first_name']
+        last_name = self.validated_data['last_name']
+        email = self.validated_data['email']
+        password = self.validated_data['password']
+        password2 = self.validated_data['confirm_password']
+        
+        if password != password2:
+            raise serializers.ValidationError({'error' : "Password Doesn't Match"})
+        
+        if User.objects.filter(email = email).exists():
+            raise serializers.ValidationError({'error' : "Email Already Exists"})
+        
+        account = User(username = username, email = email, first_name = first_name, last_name = last_name)
+        account.set_password(password)
+        account.is_active = False
+        account.save()
+        return account
+    
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required = True)
+    password = serializers.CharField(required = True)
--- rosarium_server/settings.py
+++ rosarium_server/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'all_products',
     'contact',
     'customer',
@@ -128,3 +132,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
