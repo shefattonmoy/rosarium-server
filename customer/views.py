c4f8b46878d39f7c2a7cac3b214505705e245546
shefattonmoy <shefathossain7@gmail.com>
2024-08-13 17:24:14 +0600

    updated

--- customer/views.py
+++ customer/views.py
@@ -28,7 +28,7 @@ class UserRegistrationApiView(APIView):
             user = serializer.save()
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f"http://127.0.0.1:8000/customer/active/{uid}/{token}"
+            confirm_link = f"https://rosarium-server.onrender.com/customer/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
             email = EmailMultiAlternatives(email_subject, '', to = [user.email])
